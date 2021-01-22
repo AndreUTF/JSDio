725b10ea97f810e296da8096459d41e498c82fc1
André Portes Ferriani <andre.portes17@gmail.com>
2021-01-22 13:20:07 -0300

    dasd

--- js/main.js
+++ js/main.js
@@ -37,3 +37,42 @@ var count = 5;
 for (var j = 0; j < count; j++) {
     console.log(j);
 }
+
+var str = ["mae", "floresta", "avo"]
+str.join(" > ");
+console.log(str);
+
+function soma(n1, n2) {
+    return n1 + n2;
+}
+
+alert(soma(5, 10));
+
+function validaIdade(idade) {
+    var validar;
+    if (idade > 18) {
+        validar = true;
+    } else {
+        validar = false;
+    }
+    return validar;
+}
+
+function clicou() {
+    //alert("Obrigado por clicar");
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar<b>";
+    //console.log(document.getElementById("agradecimento").innerHTML)
+}
+
+function redirecionar() {
+    window.open("https://globallabs.academy/");
+    //window.location.href = "https://www.hltv.org";
+}
+
+function trocar() {
+    document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    //alert("trocar contexto");
+}
+
+var idade_entry = prompt("Qual a sua idade?");
+console.log(validaIdade(idade_entry));
