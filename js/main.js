9e163918f99a9e9b12c9ba2aa602b50fc04c0795
André Portes Ferriani <andre.portes17@gmail.com>
2021-01-22 00:37:24 -0300

    asd

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ var idade = 29;
 var idade1 = 10;
 var frase = "Japao é o melhor time do mundo";
 
+/*
 alert("Bem vindo " + nome);
 alert(nome + " tem " + idade + " anos");
 alert(idade + idade1);
@@ -15,3 +16,24 @@ console.log(lista[1]);
 console.log(lista[2]);
 lista.push("morango");
 console.log(lista);
+
+var dicionarioFruta = [{ nome: "maca", cor: "vermelha" }, { nome: "abacaxi", cor: "amarelo" }];
+console.log(dicionarioFruta);
+alert(dicionarioFruta);
+
+alert(dicionarioFruta[1].nome);
+
+
+var idade3 = prompt("Qual sua idade?");
+if (idade3 >= 18) {
+    alert("maior de idade");
+} else {
+    alert("menor de idade");
+}
+*/
+
+
+var count = 5;
+for (var j = 0; j < count; j++) {
+    console.log(j);
+}
