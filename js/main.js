55dc60ef6ae0b34eb8a05b8489d392418d6dd151
André Portes Ferriani <andre.portes17@gmail.com>
2021-01-22 00:09:31 -0300

    Comandos basicos de JS

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var nome = "andré portes ferriani";
+var idade = 29;
+var idade1 = 10;
+var frase = "Japao é o melhor time do mundo";
+
+alert("Bem vindo " + nome);
+alert(nome + " tem " + idade + " anos");
+alert(idade + idade1);
+console.log(idade + idade1);
+console.log(frase.replace("Japao", "Brasil"));
+
+var lista = ["maca", "pera", "laranja"];
+console.log(lista[0]);
+console.log(lista[1]);
+console.log(lista[2]);
+lista.push("morango");
+console.log(lista);
