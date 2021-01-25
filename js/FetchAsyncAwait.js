045d91468a68c9040bf800ec0d230d20668c6fe3
André Portes Ferriani <andre.portes17@gmail.com>
2021-01-25 18:15:37 -0300

    fetch added

--- js/FetchAsyncAwait.js
+++ js/FetchAsyncAwait.js
@@ -1,3 +1,30 @@
-fetch('C:\JSworkspace\JSDio\data').then(responseStream => {
+/*
+fetch('js/data.json').then(responseStream => {
     console.log(responseStream);
 });
+*/
+
+/*
+fetch('js/data.json')
+    .then(responseStream => {
+        responseStream.json().then(data => {
+            console.log(data);
+        });
+    });
+*/
+
+
+fetch('js/data12.json')
+    .then(responseStream => {
+        if (responseStream.status === 200) {
+            return responseStream.json();
+        } else {
+            throw new Error('Request error');
+        }
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(err => {
+        console.log('Error: ', err);
+    });
