8d3368ff917aefce81c2acfca152caa35eb60213
André Portes Ferriani <andre.portes17@gmail.com>
2021-01-26 12:28:56 -0300

    Fetch side updated

--- js/FetchAsyncAwait.js
+++ js/FetchAsyncAwait.js
@@ -28,3 +28,41 @@ fetch('js/data12.json')
     .catch(err => {
         console.log('Error: ', err);
     });
+
+const asyncTimer = () =>
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(12345);
+        }, 1000);
+    });
+
+const simpleFunc = async() => {
+    const data = await asyncTimer();
+    const dataJSON = await fetch('js/data.json').then(resStream =>
+        resStream.json()
+    );
+    return dataJSON;
+};
+
+simpleFunc()
+    .then(data => {
+        console.log(data);
+    })
+    .catch(err => {
+        console.log(err);
+    })
+
+
+const EventEmitter = require('events');
+class Users extends EventEmitter {
+    userLogged(data) {
+        this.emit('user Logged', { user: 'Andre Ferriani' })
+    }
+}
+
+const users = new Users();
+users.on('User logges', data => {
+    console.logd(data);
+});
+
+Users.userLogged({ user: 'Andre ferriani' })
